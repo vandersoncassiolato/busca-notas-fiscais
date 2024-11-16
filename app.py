c6c6abfc15625d6335c559a9c0a5c359f5f0be8f
vandersoncassiolato <44876224+vandersoncassiolato@users.noreply.github.com>
2024-11-16 07:38:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -355,7 +355,7 @@ def main():
             
             div.stButton > button {
                 margin-top: 1px;
-                height: 35px;  /* Ajuste essa altura conforme necessário */
+                height: 30px;  /* Ajuste essa altura conforme necessário */
             }
             </style>
         """, unsafe_allow_html=True)
